e44fc0d9f6c3beb9d3fb635d765d67a774ca79aa
DocPhiSch <philipp.schoengraf@googlemail.com>
2025-02-20 08:32:04 +0100

    streamlit scripts single player and group recommendation on baseline model added

---
+++ source/streamlit_diceandadvice_baseline_groups.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+
+from baseline_model import get_recommendations, get_group_recommendations
+
+# read full board games information file from csv
+df_games = pd.read_csv('data/boardgames_ranks.csv')
+
+st.title("Dice and Advice")
+st.write("Welcome to Game (K)Nights' Dice and Advice! Our novel tool uses ai to recommend the best game for you and friends. Please enter your username and your favorite boardgame")
+
+
+player_number = st.slider("How many players are you? (1-10)", min_value=0, max_value=10)
+
+if player_number > 0:
+    st.write("You are " + str(player_number) + " players. Please choose your favorite games." )
+
+    player_list = []
+    game_list = []
+    for i in range(0, player_number):
+        player_name = st.text_input("Enter your user name player"+str(i+1),key="pn"+str(i))
+        player_list.append(player_name) 
+        
+        # Create a search input for the game name
+        search_text_i = st.text_input("Search for a board game:", key="g"+str(i))
+        # If there is input, filter the games in df_games
+        if search_text_i:
+            # Filter the games based on the search input (case insensitive)
+            filtered_games = df_games[df_games['name'].str.contains(search_text_i, case=False, na=False)]
+
+            # If there are matching results, show them in a selectbox
+            if not filtered_games.empty:
+                game_i = st.selectbox("Select a game", filtered_games['name'].tolist())
+                game_list.append(game_i) 
+                #st.write(f"You selected: {game}")
+            else:
+                st.write("No games found matching that search.")
+        else:
+            st.write("Enter the board game name or a part of it and press Enter to open a selection list")
+
+    if player_list:
+        st.write("You selected:", game_list)
+
+if st.button("Get recommendations"):
+    recommendations = get_group_recommendations(game_titles=game_list)
+    #if recommendations:
+    st.write('Hi, the Top 10 recommended boardgames based on your selections "', game_list ,'" are:')
+    # st.write(recommendations[['name', 'description']])
+    st.table(recommendations[['name', 'description']])
