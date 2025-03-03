e3f79869aa5a9e27d079f9836360869b9f55f6bb
StefanDah <stefan.dahlh@googlemail.com>
2025-03-03 09:32:35 +0100

    1st draft

--- source/pages/3_single_recommendation.py
+++ source/pages/3_single_recommendation.py
@@ -1,6 +1,12 @@
 # imports
 import pandas as pd
 import streamlit as st
+import numpy as np
+import tensorflow as tf
+import tensorflow_recommenders as tfrs
+
+from model_utils import get_recommendation
+                  
 
 st.title('Single Player Recommendation Page under Construction')
 
@@ -10,3 +16,14 @@ if "ratings_df" in st.session_state and not st.session_state["ratings_df"].empty
     st.dataframe(st.session_state["ratings_df"])
 else:
     st.warning("No ratings found. Please add ratings.")
+
+
+
+input = st.text_input("Enter your favorite boardgame:")
+
+if input:
+    st.write(f"You entered: {input}")     
+
+    game_ids, game_names, query_features = get_recommendation('input')
+
+    st.write(f'You should play: {game_names}')
